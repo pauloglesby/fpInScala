9e189bc4750266ce54abf00a6b86d3281565ebfd
pauloglesby <paul.oglesby@gmail.com>
2016-04-18 02:18:53 +0100

    Implement `Compose`.

--- ch2/src/main/scala/ly/analogical/fpInScala/Compose.scala
+++ ch2/src/main/scala/ly/analogical/fpInScala/Compose.scala
@@ -9,6 +9,8 @@ object Compose {
     * Ex 2.5: Implement the HOF that composes two functions.
     */
 
-  def compose[A, B, C](f: B => C, g: A => B): A => C = ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C = { a: A =>
+    f(g(a))
+  }
 
 }
