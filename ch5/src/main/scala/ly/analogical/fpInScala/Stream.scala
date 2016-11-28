49101e2c5df1858f76662bc6813efccd9f59bcb6
Paul Oglesby <paul.oglesby@bgch.co.uk>
2016-11-28 18:11:07 +0000

    Introduce `take` and `drop`

--- ch5/src/main/scala/ly/analogical/fpInScala/Stream.scala
+++ ch5/src/main/scala/ly/analogical/fpInScala/Stream.scala
@@ -1,6 +1,10 @@
 package ly.analogical.fpInScala
 
+import scala.annotation.tailrec
+
 sealed trait Stream[+A] {
+
+  import Stream._
 
   // NB: Explicit forcing of the h thunk using h()
   def headOption: Option[A] = this match {
@@ -16,6 +20,22 @@ sealed trait Stream[+A] {
   def toList: List[A] = this match {
     case Empty => Nil
     case Cons(h, t) => h() :: t().toList
+  }
+
+  /**
+    * Ex 5.2
+    * Write the function take(n) for returning the first n elements of a Stream,
+    * and drop(n) for skipping the first n elements of a Stream.
+    */
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
   }
 
 }
--- ch5/src/test/scala/ly/analogical/fpInScala/StreamTests.scala
+++ ch5/src/test/scala/ly/analogical/fpInScala/StreamTests.scala
@@ -14,4 +14,32 @@ class StreamTests extends BaseSpec {
 
   }
 
+  describe("`take` should") {
+
+    it("return an `Empty` when called on `Empty`") {
+      Empty.take(1) should equal(Empty)
+    }
+
+    it("return the first n elements of the Stream as a Stream") {
+      // NB need to evaluate the stream to test it, so use `toList`!
+      // otherwise we just end up looking at uncalled anonymous functions
+      Stream(1, 2, 3, 4).take(3).toList should equal(List(1, 2, 3))
+    }
+
+  }
+
+  describe("`drop` should") {
+
+    it("return an `Empty` when called on `Empty`") {
+      Empty.drop(1) should equal(Empty)
+    }
+
+
+    it("return the Stream succeeding the first n elements as a Stream") {
+      val test = Stream(1, 2, 3, 4).drop(2)
+      test.toList should equal(List(3, 4))
+    }
+
+  }
+
 }
